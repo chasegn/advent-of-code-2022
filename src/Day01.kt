3cbbc5f88a845ff2c3cc255fc50129a5917211b6
Galen Chase <chasegn@gmail.com>
2022-12-01 19:13:37 -0700

    Completed solution for Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
+import java.lang.Integer.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var curSum = 0
+        var savedMax = 0
+
+        for (weight in input) {
+            if (weight.isNotEmpty()) {
+                curSum += weight.toInt()
+            } else {
+                savedMax = max(savedMax, curSum)
+                curSum = 0
+            }
+        }
+
+        return savedMax
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = mutableListOf<Int>()
+        var curSum = 0
+
+        for (weight in input) {
+            if (weight.isNotEmpty()) {
+                curSum += weight.toInt()
+            } else {
+                list.add(curSum)
+                curSum = 0
+            }
+        }
+
+        list.sortDescending()
+
+        return list.slice(0..2).sumOf { it }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_Test")
+    val testResult = part1(testInput)
+//    println(testResult)
+    check(testResult == 24000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 69501
+    println(part2(input)) // 202346
 }
