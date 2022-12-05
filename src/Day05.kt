6cbb7b355603c168f01b4ab6de97d3f23a957ea5
Galen Chase <chasegn@gmail.com>
2022-12-04 22:19:02 -0700

    Base files for Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,22 @@
+
+/**
+ * Day 05 for Advent of Code 2022
+ * https://adventofcode.com/2022/day/5
+ */
+class Day05 : Day {
+    override val inputFileName: String = "Day05"
+    override val test1Expected: Int = -1
+    override val test2Expected: Int = -1
+
+    override fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    /**
+     * Accepted solution:
+     */
+    override fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+}
