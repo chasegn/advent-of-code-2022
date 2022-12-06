f728ca371cdcc3d1dec765027dfe1dfc040b21f7
Galen Chase <chasegn@gmail.com>
2022-12-05 17:46:29 -0700

    Complete part 1.

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,4 @@
+import java.util.*
 
 /**
  * Day 05 for Advent of Code 2022
@@ -5,11 +6,48 @@
  */
 class Day05 : Day {
     override val inputFileName: String = "Day05"
-    override val test1Expected: Int = -1
-    override val test2Expected: Int = -1
+    override val test1Expected: String = "CMZ"
+    override val test2Expected: String? = null
 
-    override fun part1(input: List<String>): Int {
-        return input.size
+    /**
+     * Accepted solution: "CNSZFDVLJ"
+     */
+    override fun part1(input: List<String>): String {
+        val prep = LinkedList<String>()
+        val iter = input.listIterator()
+        var line = iter.next()
+
+        do {
+            prep.add(line)
+            line = iter.next()
+        } while (line.contains('['))
+        // after loop, line should be line with numbers
+
+        val stacks = mutableListOf<Stack<Char>>()
+        val stackCount = line[line.lastIndex].digitToInt()
+        for (i in 1..stackCount) {
+            stacks.add(Stack<Char>())
+        }
+
+        prep.reversed().forEach {
+            buildStacks(stacks, it)
+        }
+
+        iter.next()
+
+        do {
+            moveCrates(stacks, iter.next())
+        } while(iter.hasNext())
+
+        var result = ""
+        stacks.forEach{
+            result += when (it.size) {
+                0 -> ' '
+                else -> it.peek()
+            }
+        }
+
+        return result
     }
 
     /**
@@ -19,4 +57,35 @@ class Day05 : Day {
         return input.size
     }
 
+    private fun moveCrates(stacks: List<Stack<Char>>, instruction: String) {
+        val instructionParts = instruction.split(' ')
+        val qty = instructionParts[1].toInt()
+        val src = stacks[instructionParts[3].toInt() - 1]
+        val dest = stacks[instructionParts[5].toInt() - 1]
+
+        for (i in 1..qty) {
+            dest.push(src.pop())
+        }
+    }
+
+    private fun buildStacks(stacks: List<Stack<Char>>, line: String) {
+        var stackPos = 0 // counts stack index
+        var charPos = 0  // counts between crate character locations
+        val iter = line.iterator()
+
+        while (iter.hasNext()) {
+            val c = iter.next()
+
+            if (c != '[' && c != ']' && c != ' ') {
+                if (c.isUpperCase() || c.isLowerCase()) {
+                    stacks[stackPos].push(c)
+                }
+            }
+
+            charPos++
+            if (charPos > 4 && charPos % 4 == 1) {
+                stackPos++
+            }
+        }
+    }
 }
