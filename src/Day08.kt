adb0f546811a51b68993f3bd336053245af3e0c5
Galen Chase <chasegn@gmail.com>
2022-12-08 01:45:38 -0700

    Day 8 part 2 completed

--- src/Day08.kt
+++ src/Day08.kt
@@ -5,9 +5,10 @@
  */
 class Day08 : Day {
     override val inputFileName: String = "Day08"
-    override val test1Expected: Int? = null
-//    override val test1Expected: Int = 21
-    override val test2Expected: Int? = null
+//    override val test1Expected: Int? = null
+    override val test1Expected: Int = 21
+//    override val test2Expected: Int? = null
+    override val test2Expected: Int = 8
 
     /**
      * Accepted solution: 1711
@@ -18,24 +19,34 @@ class Day08 : Day {
 
         for (i in 1 until trees.size-1) {
             for (j in 1 until trees.size-1) {
-                print("$i,$j: ${trees[i]!![j]}")
+//                print("$i,$j: ${trees[i]!![j]}")
                 if (isVisible(trees, i, j)) {
                     result++
-                    print(" vis")
+//                    print(" vis")
                 }
-                print("\n")
+//                print("\n")
             }
         }
 
-        print("\n")
+//        print("\n")
         return result + ((trees[0]!!.size - 1)*2 + (trees.size - 1)*2)
     }
 
     /**
-     * Accepted solution:
+     * Accepted solution: 301392
      */
     override fun part2(input: List<String>): Int {
-        return input.size
+        val trees = buildTreeGrid(input)
+        var max = 0
+
+        for (i in trees.indices) {
+            for (j in trees.indices) {
+                val score = scenicValue(trees, i, j)
+                max = max.coerceAtLeast(score)
+            }
+        }
+
+        return max
     }
 
     private fun buildTreeGrid(input: List<String>): Array<Array<Int>?> {
@@ -49,7 +60,7 @@ class Day08 : Day {
     }
 
     private fun isVisible(trees: Array<Array<Int>?>, x: Int, y: Int): Boolean {
-        val curHeight = (trees[x]?.get(y))!!
+        val curHeight = trees[x]!![y]
 
         // quick check if neighbors are all taller
         var blockedNorth = trees[x-1]!![y] >= curHeight
@@ -99,5 +110,43 @@ class Day08 : Day {
         // can exit early if any ONE direction fails
 
         return !blockedNorth || !blockedWest || !blockedEast || !blockedSouth
+    }
+
+    private fun scenicValue(trees: Array<Array<Int>?>, x: Int, y: Int): Int {
+        val curHeight = trees[x]!![y]
+        var scoreN = 0
+        var scoreW = 0
+        var scoreE = 0
+        var scoreS = 0
+
+        for (i in (x-1) downTo 0) {
+            scoreN++
+            if (trees[i]!![y] >= curHeight) {
+                break
+            }
+        }
+
+        for (i in (y-1) downTo 0) {
+            scoreW++
+            if (trees[x]!![i] >= curHeight) {
+                break
+            }
+        }
+
+        for (i in (y+1) until trees.size) {
+            scoreE++
+            if (trees[x]!![i] >= curHeight) {
+                break
+            }
+        }
+
+        for (i in (x+1) until trees.size) {
+            scoreS++
+            if (trees[i]!![y] >= curHeight) {
+                break
+            }
+        }
+
+        return scoreN * scoreW * scoreE * scoreS
     }
 }
