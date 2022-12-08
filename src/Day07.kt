cd7d705aa60bf565f8c04a6b747f6c001847e957
Galen Chase <chasegn@gmail.com>
2022-12-07 22:59:36 -0700

    Class and data files for Day 7

---
+++ src/Day07.kt
@@ -0,0 +1,24 @@
+
+/**
+ * Day 07 for Advent of Code 2022
+ * https://adventofcode.com/2022/day/7
+ */
+class Day07 : Day {
+    override val inputFileName: String = "Day07"
+    override val test1Expected: Int = -1
+    override val test2Expected: Int = -1
+
+    /**
+     * Accepted solution:
+     */
+    override fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    /**
+     * Accepted solution:
+     */
+    override fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
